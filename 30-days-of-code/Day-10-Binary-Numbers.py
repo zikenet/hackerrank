90cb9025f91ca64feb2bed60b8e5e7995ad146b3
zikenet <zikenet116@gmail.com>
2021-03-30 21:26:47 +0300

    updated

--- 30-days-of-code/Day-10-Binary-Numbers.py
+++ 30-days-of-code/Day-10-Binary-Numbers.py
@@ -8,4 +8,20 @@ import re
 import sys
 
 if __name__ == '__main__':
-    n = int(raw_input())
+    
+    N = int(input())
+    DATA = bin(N)
+    
+    MAXIMUM = 0
+    CURRENT = 0
+    
+    for num in DATA:
+        
+        if num == '1':
+            CURRENT = CURRENT + 1
+        
+        else:
+            MAXIMUM = max(MAXIMUM, CURRENT)
+            CURRENT = 0
+
+print(max(MAXIMUM, CURRENT))
