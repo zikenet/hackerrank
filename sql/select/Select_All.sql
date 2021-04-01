58cf476ff102a5837d1cda191059b9e4c85e7494
zikenet <zikenet116@gmail.com>
2021-04-01 19:03:01 +0300

    Day 14 and sql

---
+++ sql/select/Select_All.sql
@@ -0,0 +1,16 @@
+Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows: 
++-------------+-------------+
+| Field       | Type        |
++-------------+-------------+
+| ID          | NUMBER      |
+| NAME        | VARCHAR(17) |
+| COUNTRYCODE | VARCHAR(3)  |
+| DISTRICT    | VARCHAR(20) |
+| POPULATION  | NUMBER      |
++-------------+-------------+
+
+SOLUTION:
+
+SELECT * FROM CITY;
